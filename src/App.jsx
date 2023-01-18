3f83f64aa684ca7eef63d5399657db254b8fc5ac
Leonardo <leonardo0507.henrique@gmail.com>
2023-01-18 09:02:32 -0300

    leo-react-app configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import "./scss/style.scss";
-import SVGReact from "./assets/react.svg";
 
 export default function App() {
     return (
-		<main className="app">
-			<div className="app__react">
-				<a href="https://reactjs.org/"
-				target="_blank"
-				rel="external noopener noreferrer">
-					<SVGReact />
-				</a>
-			</div>
-
-			<h1 className="app__title"
-			translate="no">Léo React App</h1>
-
-			<a className="btn"
-			href="https://github.com/Leo-Henrique/leo-react-app"
-			target="_blank">
-				Access repository
-			</a>
-		</main>
+		<p>Hello World!</p>
 	);
 }
