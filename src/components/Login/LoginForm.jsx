145c708f96181d3677729099012683cd666e5c18
Leonardo <leonardo0507.henrique@gmail.com>
2023-01-25 09:48:02 -0300

    Reactivity for created login fields. Changed header icons.

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,5 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function LoginForm() {
-    return <>LoginForm</>;
+    const form = {
+        username: "",
+        password: "",
+    };
+    const [login, setLogin] = React.useState(form);
+    const handleChange = ({ target: { value, id } }) => {
+        setLogin({ ...login, [id]: value });
+    };
+    const handleSubmit = async () => {
+        const api = "https://dogsapi.origamid.dev/json/jwt-auth/v1/token";
+        const options = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(login),
+        };
+        const res = await fetch(api, options);
+        const data = await res.json();
+
+        console.log(data);
+    };
+
+    return (
+        <section>
+            <h1>Entrar</h1>
+
+            <form>
+                {Object.keys(form).map((id) => (
+                    <input
+                        key={id}
+                        id={id}
+                        name={id}
+                        placeholder={id}
+                        onChange={handleChange}
+                    />
+                ))}
+
+                <button type="button" onClick={handleSubmit}>
+                    Entrar
+                </button>
+            </form>
+
+            <Link to="/login/register">Cadastre-se</Link>
+        </section>
+    );
 }
