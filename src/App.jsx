a0c16eedf827a425a0867098a5aa2a380bab93b4
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-02-27 16:18:58 -0300

    Robust transitions added on route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./scss/style.scss";
 import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -10,11 +10,27 @@ import LeoAnimate from "leo-animate.js";
 
 export default function App() {
     const location = useLocation();
+    const navigation = useNavigate();
 
     React.useEffect(() => {
-        const options = { transitions: { duration: "400ms" } }
+        const duration = 200;
+        const options = { transitions: { duration: `${duration}ms` } };
+        const links = document.querySelectorAll("a");
+        const change = (event) => {
+            const animate = document.querySelector("[data-animate]");
+            const { href } = event.currentTarget;
+            const { origin } = window.location;
+            const path = href.replace(origin, "");
+
+            event.preventDefault();
+            animate.classList.remove("--animated");
+            setTimeout(() => navigation(path), duration);
+        }
+
+        links.forEach(link => link.addEventListener("click", change));
 
         new LeoAnimate(options);
+        return () => links.forEach(link => link.removeEventListener("click", change));
     }, 
     [location])
 
